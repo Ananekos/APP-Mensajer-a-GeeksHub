0c04deb1cb60733a77a4c03d34c51a1323cd8dce
Ananekos <103376976+Ananekos@users.noreply.github.com>
2022-05-20 16:21:10 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,20 +1,18 @@
 const btnAñadirCanales = document.querySelector(".btn_añadir_canales");
-var resultadoPrm;
+const btnAñadirMensaje = document.querySelector(".btn_añadir_canales");
 var arrCanales = [];
 
 
 
 btnAñadirCanales.addEventListener("click", function (evento){
-	
-	for (var i =0; i<=0; i++){
-		arrCanales.push(prompt("Nombre del nuevo canal:"));
-		console.log(arrCanales);
-		nuevoItem = arrCanales[i];
-		var lista = document.createElement("li");
-		lista.innerHTML = nuevoItem.value;
-		listaCanales.appendChild(lista);
-        nuevoItem.value = "";
-	}
+    arrCanales.push(prompt("Nombre del nuevo canal:"));
+    var lista = document.createElement("li");
+    for (var i =0; i<arrCanales.length; i++){
+        console.log('arrayCanales', arrCanales);
+        nuevoItem = arrCanales[i];
+        lista.innerHTML = nuevoItem;
+        listaCanales.appendChild(lista);
+		
+    }
 });
-
 
