84357ab6d1c723e908af3a06c053310d421944ef
Ananekos <103376976+Ananekos@users.noreply.github.com>
2022-05-23 11:27:41 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
 const btnAñadirCanales = document.querySelector(".btn_añadir_canales");
-const btnAñadirMensaje = document.querySelector(".btn_añadir_canales");
-var arrCanales = [];
-
-
+let arrCanales = [];
 
 btnAñadirCanales.addEventListener("click", function (evento){
     arrCanales.push(prompt("Nombre del nuevo canal:"));
-    var lista = document.createElement("li");
-    for (var i =0; i<arrCanales.length; i++){
+    let lista = document.createElement("li");
+    for (let i =0; i<arrCanales.length; i++){
         console.log('arrayCanales', arrCanales);
         nuevoItem = arrCanales[i];
         lista.innerHTML = nuevoItem;
@@ -16,3 +13,26 @@ btnAñadirCanales.addEventListener("click", function (evento){
     }
 });
 
+
+var nombreUsuario = document.getElementById("nom_usuario").innerHTML;
+var fecha = new Date();
+
+console.log(fecha.getHours());
+console.log(fecha.getMinutes());
+console.log(fecha.getSeconds());
+console.log(fecha.getDay());
+console.log(fecha.getDate());
+console.log(fecha.getMonth());
+console.log(fecha.getFullYear());
+  
+function sendMessage() {
+  var message = document.getElementById("typing-box").value;
+  var html = nombreUsuario + fecha +
+        '<div class="message-box my-message-box">' +
+        '<div class="message my-message"> ' + message + ' </div>' +
+        '<div class="separator"></div>'
+
+  document.getElementById("message-area").innerHTML += html;
+  document.getElementById("typing-box").value = "";
+  
+}
