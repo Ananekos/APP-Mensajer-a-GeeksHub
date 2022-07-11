616c4ce55b5fe83e272cbc06765590519b9bd21f
Ananekos <103376976+Ananekos@users.noreply.github.com>
2022-07-11 12:59:56 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -6,12 +6,12 @@ console.log(channels);
 
 //01. CREACIÓN CANALES
 
-function capturarcanal() {
+function creatingChannels() {
 
-  let inputNuevoCanal = prompt("Nombre Nuevo Canal", "Nuevo Canal");
+  let inputNewChannel = prompt("Nombre Nuevo Canal", "Nuevo Canal");
 
   const newChannel = {
-    name: inputNuevoCanal,
+    name: inputNewChannel,
     id: 'canal-' + channelIndex,
     pos: channelIndex,
     messages: [],
@@ -20,29 +20,29 @@ function capturarcanal() {
   channels.push(newChannel);
   console.log('Canales', channels);
 
-  let botonNuevoCanal = '<button class="btnNomCanal" onclick="openChannel(' + newChannel.pos + ')" id="' + newChannel.id + '">' + newChannel.name + '</button>';
-  listaCanales.innerHTML += botonNuevoCanal;
+  let btnNewChannel = '<button class="btn_channel_name" onclick="openChannel(' + newChannel.pos + ')" id="' + newChannel.id + '">' + newChannel.name + '</button>';
+  channelsList.innerHTML += btnNewChannel;
   currentChannel = newChannel.pos;
 
   //02. MOSTRANDO LA INTERFAZ COMPLETA AL CREAR NUEVO CANAL
 
   if (channels.length == 1) {
-    document.getElementById("area_escritura").innerHTML =
-      '<div class="area_escritura" >' +
-      '<button class="boton_enviar" id="boton_enviar" onclick="enviarMensaje()"><i class="fa fa-plus"aria-hidden="true"></i></button>' +
-      '<textarea class="caja_escritura" id="caja_escritura"></textarea>' +
+    document.getElementById("writing_area").innerHTML =
+      '<div class="writing_area" >' +
+      '<button class="btn_send" id="btn_send" onclick="enviarMensaje()"><i class="fa fa-plus"aria-hidden="true"></i></button>' +
+      '<textarea class="input_box" id="input_box"></textarea>' +
       '</div>';
-    document.getElementById("area_cabecera").innerHTML =
-      '<div id=toda_cabecera>' +
-        '<div id=nom_cabecera class="nom_cabecera">Nombre del canal</div>' +
+    document.getElementById("head_container").innerHTML =
+      '<div id=all_header>' +
+        '<div id=channel_name_head class="channel_name_head">Nombre del canal</div>' +
         '<br>' +
         '<d4>Tú, Olivia Montes, Isabel Carballo</d4>' +
       '</div>' +
 
-      '<div id="todo_busqueda">' +
-        '<div class="barra_busqueda">' +
-          '<button id="boton_busqueda" class="boton_busqueda" onclick="search()"><i class="fa fa-search" aria-hidden="true"></i></button>' +
-          '<input type="text" id="input_busqueda" class="input_busqueda">' +
+      '<div id="all_search">' +
+        '<div class="search_bar">' +
+          '<button id="search_button" class="search_button" onclick="search()"><i class="fa fa-search" aria-hidden="true"></i></button>' +
+          '<input type="text" id="input_search" class="input_search">' +
         '</div>' +
       '</div>'
   }
@@ -54,31 +54,30 @@ function capturarcanal() {
 //03. ABRIENDO NUEVOS CANALES
 
 function openChannel(ChannelPos) {
-  document.getElementById("nom_cabecera").innerHTML = channels[ChannelPos].name;
+  document.getElementById("channel_name_head").innerHTML = channels[ChannelPos].name;
   currentChannel = ChannelPos;
   console.log('Open Channel', channels[ChannelPos]);
-  document.getElementById("area_mensajes_nuevos").innerHTML =
-    '<div id="nom_interlocutor"><img src="IMG/iconito_2.png" class="iconito_usuario">' +
+  document.getElementById("new_messages_area").innerHTML =
+    '<div id="nom_interlocutor"><img src="IMG/iconito_2.png" class="user_icon">' +
     '<d2> Olivia Montes </d2>' +
     '<d3>12:15:23</d3>' +
     '<d3> | 5 de Febrero del 2022</d3>' +
     '</div>' +
-    '<div class="caja_mensajes otros-caja_mensajes">' +
-    '<div class="mensaje otros-mensaje"> Hola, ¿qué tal?. Bienvenid@ al canal.</div>';
+    '<div class="mesages_box otros-mesages_box">' +
+    '<div class="mensaje other_user_message"> Hola, ¿qué tal?. Bienvenid@ al canal.</div>';
 
   if (channels[ChannelPos].messages.length > 0) {
     for (let message of channels[ChannelPos].messages) {
       pintarMensaje(message);
     }
   }
-
 }
 
 //04. BUSCANDO MENSAJES ENVIADOS
 
 function search() {
   let results = '';
-  let value = document.getElementById("input_busqueda").value;
+  let value = document.getElementById("input_search").value;
   let listaMensajes = channels[currentChannel].messages;
   for (let messageItem of listaMensajes) {
     if (messageItem.mensaje.includes(value)) {
@@ -100,24 +99,24 @@ let mesesDelAño = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Juli
 
   let actualizarHora = function () {
     fecha = new Date(),
-      horas = fecha.getHours(),
-      minutos = fecha.getMinutes(),
-      segundos = fecha.getSeconds(),
-      dia = fecha.getDate(),
-      mes = fecha.getMonth(),
+      hours = fecha.getHours(),
+      minutes = fecha.getMinutes(),
+      seconds = fecha.getSeconds(),
+      day = fecha.getDate(),
+      month = fecha.getMonth(),
       year = fecha.getFullYear(),
 
 
-      phoras = document.getElementById("horas").innerHTML = horas,
-      pminutos = document.getElementById("minutos").innerHTML = minutos,
-      psegundos = document.getElementById("segundos").innerHTML = segundos,
-      pdia = document.getElementById("dia").innerHTML = dia,
-      pmes = document.getElementById("mes").innerHTML = mesesDelAño[mes],
+      phours = document.getElementById("hours").innerHTML = hours,
+      pminutes = document.getElementById("minutes").innerHTML = minutes,
+      pseconds = document.getElementById("seconds").innerHTML = seconds,
+      pday = document.getElementById("day").innerHTML = day,
+      pmonth = document.getElementById("month").innerHTML = mesesDelAño[month],
       pyear = document.getElementById("year").innerHTML = year;
 
-    if (phoras < 10) { phoras = "0" + phoras };
-    if (pminutos < 10) { pminutos = "0" + pminutos };
-    if (psegundos < 10) { psegundos = "0" + psegundos };
+    if (phours < 10) { phours = "0" + phours };
+    if (pminutes < 10) { pminutes = "0" + pminutes };
+    if (pseconds < 10) { pseconds = "0" + pseconds };
 
   };
 
@@ -130,10 +129,10 @@ let mesesDelAño = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Juli
 
 function enviarMensaje() {
   console.log('enviarMensaje', channels[currentChannel]);
-  let mensajeEnviado = document.getElementById("caja_escritura").value;
+  let mensajeEnviado = document.getElementById("input_box").value;
   if (mensajeEnviado != '') {
-    let reloj = (phoras + ":" + pminutos + ":" + psegundos + " | " + pdia + " de " + pmes + " del " + pyear);
-    let usuarioCapturar = document.getElementById("nom_usuario").innerHTML = "Alumno@ BTC";
+    let reloj = (phours + ":" + pminutes + ":" + pseconds + " | " + pday + " de " + pmonth + " del " + pyear);
+    let usuarioCapturar = document.getElementById("user_name").innerHTML = "Alumno@ BTC";
 
     const newMessage = {
       usuario: usuarioCapturar,
@@ -149,18 +148,13 @@ function enviarMensaje() {
 
 function pintarMensaje(newMessage) {
   let html =
-  '<div id="nom_interlocutor"><img src="IMG/iconito_1.png" class="iconito_usuario">' +
+  '<div id="nom_interlocutor"><img src="IMG/iconito_1.png" class="user_icon">' +
   '<d2>' + newMessage.usuario + '</d2>' +
   ' <d3>' + newMessage.fecha + '</d2>' +
-  '<div class="mensaje otros-mensaje"> ' + newMessage.mensaje + ' </div> ';
+  '<div class="mensaje other_user_message"> ' + newMessage.mensaje + ' </div> ';
   
     
 
-  document.getElementById("area_mensajes_nuevos").innerHTML += html;
-  document.getElementById("caja_escritura").value = "";
+  document.getElementById("new_messages_area").innerHTML += html;
+  document.getElementById("input_box").value = "";
 }
-
-' <div class=" nom_usuario "><img src="IMG/iconito_1.png" class="iconito_usuario"></div>' +
-    '<div class="usario_style">' + newMessage.usuario + "<p class='tiempo'>" + newMessage.fecha +
-    '<div class="mensaje otros-mensaje"> ' + newMessage.mensaje + ' </div> ';
-
